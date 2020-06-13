231c127fc42afbe89fed07243e90742fcbbf96fa
Shivank Goel <shivankgoel@shivanks-mbp.home>
2020-06-12 22:36:47 -0400

    database config

--- cbprojects/settings.py
+++ cbprojects/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default = '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-print(DEBUG)
+
 
 ALLOWED_HOSTS = [
     host.strip()
@@ -92,12 +92,17 @@ WSGI_APPLICATION = 'cbprojects.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.config(default=config('DATABASE_URL'))
+    }
 
 
 # Password validation
