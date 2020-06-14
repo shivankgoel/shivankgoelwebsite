54c877cf76b9a3a471d6b5c63063e5948b3bd21d
Shivank Goel <shivankgoel@shivanks-mbp.home>
2020-06-14 01:17:45 -0400

    staticfileserror

--- cbprojects/settings.py
+++ cbprojects/settings.py
@@ -21,7 +21,7 @@ from whitenoise.storage import CompressedManifestStaticFilesStorage
 class WhiteNoiseStaticFilesStorage(CompressedManifestStaticFilesStorage):
     manifest_strict = False
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
