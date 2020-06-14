d2b0ebb6c36d2fe6174e49d84362ed07fb634dba
Shivank Goel <shivankgoel@shivanks-mbp.home>
2020-06-13 21:47:55 -0400

    reverting changes for heroku

--- cbprojects/settings.py
+++ cbprojects/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'cbprojects.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if DEBUG or config('ONAWS' , default = 'True'):
+if DEBUG or config('ONAWS' , default = 'False'):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
