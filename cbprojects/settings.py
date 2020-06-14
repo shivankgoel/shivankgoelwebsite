00834d369e04edfbc83658e66f7b39032849560b
Shivank Goel <shivankgoel@shivanks-mbp.home>
2020-06-13 23:29:05 -0400

    added django heroku plugin

--- cbprojects/settings.py
+++ cbprojects/settings.py
@@ -15,6 +15,7 @@ import os
 from decouple import config
 import dj_database_url
 
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -92,6 +93,9 @@ WSGI_APPLICATION = 'cbprojects.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+
+
 
 if config('ONAWS' , default = 'False') or config('LOCAL' , default = 'False'):
     DATABASES = {
@@ -208,3 +212,6 @@ SOCIALACCOUNT_PROVIDERS = {
 #         },
 #     }
 # }
+
+
+django_heroku.settings(locals())
