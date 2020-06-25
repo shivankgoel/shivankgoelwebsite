f233c28860e17b4594681de993965ebe2a5695d9
Shivank Goel <shivankgoel@shivanks-mbp.home>
2020-06-25 18:00:27 -0400

    Added primary suthor for blog

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,17 @@ from django_mysql.models import ListTextField
 from django.core.validators import MinValueValidator, MaxValueValidator
 from autoslug import AutoSlugField
 from django.contrib.auth.models import User
+from sortedm2m.fields import SortedManyToManyField
 
 # Create your models here.
 
 class Author(models.Model):
     name = models.CharField(max_length = 256)
     slug = AutoSlugField(populate_from='name', blank = True, editable=True)
+    imageurl = models.CharField(max_length = 512, blank = True)
+    linkedin_url = models.CharField(max_length = 512, blank = True)
+    instagram_url = models.CharField(max_length = 512, blank = True)
+    facebook_url = models.CharField(max_length = 512, blank = True)
     def __str__(self):
         return self.name
 
@@ -26,7 +31,8 @@ class Article(models.Model):
     subtitle = models.CharField(max_length = 1024, blank = True)
     createdAt = models.DateTimeField(auto_now_add = True)
     content = models.TextField()
-    authors = models.ManyToManyField('Author')
+    mainauthor = models.ForeignKey('Author', on_delete = models.CASCADE, related_name = 'primary_articles', blank = True)
+    other_authors = models.ManyToManyField('Author', related_name = 'other_articles', blank = True)
     imageurl = models.CharField(max_length = 512, blank = True)
     imagecaption = models.CharField(max_length = 256, blank = True)
     tags = models.ManyToManyField('Tag', blank = True)
